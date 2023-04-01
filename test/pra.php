fcfc64ba8797e812ced907cf845dd1aeda028ea7
AO0Y <anastasia@huskers.unl.edu>
2023-03-31 21:24:20 -0500

    1

---
+++ test/pra.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <?php $x=5; echo "hello world! x=$x.";?>
+     <table width="400px" cellspacing="0px" cellpadding="0px" border="1px">
+
+<tr>
+
+<td height="35px" width="30px" bgcolor="#FFFFFF"></td>
+
+<td height="35px" width="30px" bgcolor="#000000"></td>
+
+</tr>
+
+<tr>
+
+<td height="35px" width="30px" bgcolor="#000000"></td>
+
+<td height="35px" width="30px" bgcolor="#FFFFFF"></td>
+
+</tr>
+
+</table>
+</body>
+</html>
